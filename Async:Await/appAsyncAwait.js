6481e269aa65474dd110bd768dd6450113eec345
cdsalty <soltiscd@gmail.com>
2019-07-11 11:29:56 -0400

    finsihing up with async/await using fetch

--- Async:Await/appAsyncAwait.js
+++ Async:Await/appAsyncAwait.js
@@ -1,6 +1,3 @@
-import { resolve } from "path";
-import { rejects } from "assert";
-
 // alert("hello");
 /*
 
@@ -12,16 +9,29 @@ myFunction()
   .then(res => console.log(res));
 */
 
+// async function myFunction() {
 
-async function myFunction() {
-
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve("Hello"), 1000);
-    });
-    const res = await promise; // Will wait until the promise is RESOLVED
-    return res; // returning the RESOLVED RESPONSE
-}
+//     const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => resolve("Hello"), 1000);
+//     });
+//     const res = await promise; // Will wait until the promise is RESOLVED before sending it.
+//     return res; // returning the RESOLVED RESPONSE
+// }
 
 // myFunction().then(res => console.log(res)); // res = response
-myFunction()
-  .then(res => console.log(res));
+// myFunction()
+//   .then(res => console.log(res)); // without, nothing will be returned
+
+
+
+/*
+---------------------------- MORE PRACTICE (ASYNC/AWAIT with FETCH()) ----------------------------
+*/
+
+async function getUser() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users'); // await the response of the fetch call
+  const data = await response.json();   // once fetch is called, then proceed. Only proceed if resolved.
+    return data;  // will only proceed once the second promise is resolved.
+    
+}
+getUser().then(users => console.log(users));
